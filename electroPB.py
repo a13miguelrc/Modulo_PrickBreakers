d5ad28ac30b70d9496156226129fa74534af58eb
MaikelRC <a13miguelrc@iessanclemente.net>
2015-02-19 21:26:31 +0100

    Alpha test v 0.3

--- electroPB.py
+++ electroPB.py
@@ -16,29 +16,29 @@ Empleado()
 
 
 #Servicio
-class Servicio(orm.Model):
-    _name = 'servicio'
-    _columns = {
-        'codigo':fields.integer('Codigo'),
-        'tipo':fields.selection((('REP','Reparacion'), ('E','Entrega'), ('REC','Recogida')),'Tipo'),
-        'ubicacion':fields.selection((('D','A Domicilio'), ('T','Taller')),'Ubicacion'),
-        'electrodomestico':fields.char('Electrodomestico',size=40),
-        'descripcion':fields.char('Descripcion',size=120),
-        'direccion':fields.char('Direccion',size=50),
-        'empleado_id':fields.many2one('empleado','Empleado')
-    }
-Servicio()
+#class Servicio(orm.Model):
+#   _name = 'servicio'
+#    _columns = {
+#        'codigo':fields.integer('Codigo'),
+#        'tipo':fields.selection((('REP','Reparacion'), ('E','Entrega'), ('REC','Recogida')),'Tipo'),
+#        'ubicacion':fields.selection((('D','A Domicilio'), ('T','Taller')),'Ubicacion'),
+#        'electrodomestico':fields.char('Electrodomestico',size=40),
+#        'descripcion':fields.char('Descripcion',size=120),
+#        'direccion':fields.char('Direccion',size=50),
+#        'empleado_id':fields.many2one('empleado','Empleado')
+#    }
+#Servicio()
 
 
 #FlotaFurgonetas
-class Furgoneta(orm.model):
-    _name = 'furgoneta'
-    _colums = {
-        'marca':fields.selection((('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault')), 'Marca'),
-        'modelo':fields.selection((('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo')), 'Modelo'),
-        'matricula':fields.selection((('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),'Matricula'),
-        'hora_salida':fields.date('Hora salida'),
-        'hora_llegada':fields.date('Hora llegada'),
-        'empleado_id':fields.many2one('empleado','Usuario', ondelete='cascade')
-    }
-Furgoneta()
+#class Furgoneta(orm.model):
+#    _name = 'furgoneta'
+#    _colums = {
+#        'marca':fields.selection((('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault')), 'Marca'),
+#        'modelo':fields.selection((('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo')), 'Modelo'),
+#        'matricula':fields.selection((('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),'Matricula'),
+#        'hora_salida':fields.date('Hora salida'),
+#        'hora_llegada':fields.date('Hora llegada'),
+#        'empleado_id':fields.many2one('empleado','Usuario', ondelete='cascade')
+#    }
+#Furgoneta()
