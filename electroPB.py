4591e073da8d83d74b72df61f8cc5ce6fe926e05
MaikelRC <a13miguelrc@iessanclemente.net>
2015-02-19 17:57:17 +0100

    Clases v1

--- electroPB.py
+++ electroPB.py
@@ -3,13 +3,34 @@ __author__ = 'PrickBreakers'
 from openerp.osv import fields, orm
 
 #Empleado
+class Empleado(orm.Model):
+    _name = 'empleado'
+    _columns = {
+        'nombre':fields.char('Nombre',size=20),
+        'apellidos':fields.char('Apellidos',size=50),
+        'direccion':fields.char('Dirección',size=50),
+        'fecha_nacimiento':fields.date('Fecha Nacimiento'),
+        'telefono':fields.char('Teléfono',size=9),
+        'servicio_id':fields.one2many('servicio.codigo','servicio_id','Hoja_de_Servicio')
+    }
 
 
-
+#Servicio
+class Servicio(orm.Model):
+    _name = 'servicio'
+    _columns = {
+        'codigo':fields.integer('Código'),
+        'tipo':fields.selection((('REP','Reparación'), ('E','Entrega'), ('REC','Recogida')),'Tipo'),
+        'ubicacion':fields.selection((('D','A Domicilio'), ('T','Taller')),'Ubicación'),
+        'electrodomestico':fields.char('Electrodoméstico',size=40),
+        'descripcion':fields.char('Descripción',size=120),
+        'direccion':fields.char('Dirección',size=50),
+        'empleado_id':fields.many2one('empleado','Empleado')
+    }
+Servicio()
 
 
 #FlotaFurgonetas
-
 class Flota(orm.model):
     _name = 'flota'
     _colums = {
