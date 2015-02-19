915668dccde2ef381663ec458170fc5bd030bf51
Pinchacarballos <oskar4416@gmail.com>
2015-02-19 17:55:51 +0100

    Actualizado

--- electroPB.py
+++ electroPB.py
@@ -7,7 +7,15 @@ from openerp.osv import fields, orm
 
 
 
+
 #FlotaFurgonetas
 
-
-
+class Flota(orm.model):
+    _name = 'flota'
+    _colums = {
+        'marca':fields.selection(('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault'), 'Marca'),
+        'modelo':fields.selection(('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo'), 'Modelo'),
+        'matricula':fields.selection(('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),
+        'hora_salida':fields.date('Hora salida'),
+        'hora_llegada':fields.date('Hora llegada')
+    }
