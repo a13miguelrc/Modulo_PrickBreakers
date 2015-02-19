b6379675f3c6482b8b7486be1b5c1a76c5c80df4
MaikelRC <a13miguelrc@iessanclemente.net>
2015-02-19 18:00:04 +0100

    Clases v1 update1

--- electroPB.py
+++ electroPB.py
@@ -34,9 +34,9 @@ Servicio()
 class Flota(orm.model):
     _name = 'flota'
     _colums = {
-        'marca':fields.selection(('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault'), 'Marca'),
-        'modelo':fields.selection(('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo'), 'Modelo'),
-        'matricula':fields.selection(('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),
+        'marca':fields.selection((('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault')), 'Marca'),
+        'modelo':fields.selection((('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo')), 'Modelo'),
+        'matricula':fields.selection((('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),'Matrícula'),
         'hora_salida':fields.date('Hora salida'),
         'hora_llegada':fields.date('Hora llegada')
     }
