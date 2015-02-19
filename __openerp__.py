e14d0517f02865712e9a3aee8176b7c6d3ff2a19
MaikelRC <a13miguelrc@iessanclemente.net>
2015-02-19 16:29:13 +0100

    Agregados archivos principales

---
+++ __openerp__.py
@@ -0,0 +1,39 @@
+__author__ = 'PrickBreakers'
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+
+{
+    "name": "ElectroPB",
+    "version": "0.1",
+    "category": "",
+    "depends": [
+                'base',
+                ],
+    "author": "PrickBreakers",
+    "description": "Módulo para gestionar empleados de una empresa de electrodomésticos",
+    "init_xml": [],
+    "data": [
+             'electroPB_view.xml'
+             ],
+    "demo_xml": [],
+    "installable": True,
+    "active": False,
+}
---
+++ setup.py
@@ -0,0 +1,26 @@
+__author__ = 'PrickBreakers'
+
+from setuptools import setup, find_packages
+
+setup (
+       name='electropb',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='PrickBreakers',
+       author_email='',
+
+       summary='Aplicación de gestión de usuarios de ElectroPB',
+       url='prickbreakers.es',
+       license='',
+       long_description='',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+
+       )
