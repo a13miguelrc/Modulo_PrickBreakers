6802c2c1af09a0616a0179f86b68c7fe93c13cf2
Pinchacarballos <oskar4416@gmail.com>
2015-02-20 18:04:58 +0100

    Actualizado

--- electroPB.py
+++ electroPB.py
@@ -24,10 +24,10 @@ Empleado()
 #FlotaFurgonetas
 class Furgoneta(orm.Model):
     _name = 'furgonetas.furgoneta'
-    _colums = {
+    _columns = {
         'marca':fields.selection((('marca1', 'Volkswagen'), ('marca2', 'Ford'), ('marca3', 'Renault')), 'Marca'),
         'modelo':fields.selection((('modelo1', 'Caddy'), ('modelo2', 'Transit'), ('modelo3', 'Kangoo')), 'Modelo'),
-        'matricula':fields.selection((('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')),'Matricula')
+        'matricula':fields.selection((('matric1', '6584-HMN'), ('matric1', '5327-HCD'), ('matric3', '2167-DLL')), 'Matricula')
     }
 Furgoneta()
 
